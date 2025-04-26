721d187d26806ca3624da9e53e3b6e8d3535bb6d
Mohamed <mintok2000@gmail.com>
2025-04-26 13:52:40 +0100

    add a github account

--- components/footer.py
+++ components/footer.py
@@ -6,8 +6,18 @@ def render_footer():
     
     # Pied de page
     st.markdown("""
+    <style>
+    .github-icon {
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+    }            
+    </style>
     <div class="footer">
         <p>Générateur d'expressions Cron | Développé avec ❤️ par Mohamed NDIAYE</p>
         <p class="small">Cet outil est à titre informatif uniquement. Testez toujours vos expressions cron dans votre environnement spécifique.</p>
+        <a href="https://github.com/Moesthetics-code/cron_generator" target="_blank">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" class="github-icon" />
+    </a>
     </div>
     """, unsafe_allow_html=True)
